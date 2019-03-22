3518a1e6208f568bfe508f07ddb400a2b497dae1
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-03-21 23:17:34 -0400

    adding controls for times -- step6

--- steps/6.js
+++ steps/6.js
@@ -1,9 +1,24 @@
 /*
 *  ✨ Let's make the workTime and breakTime something the user can change
 */
+
+const adders = document.getElementById('adders');
+// ✨ we cheated by hiding a div for most of these steps 
+// ✨ this is where we unhide it by setting display to 'flex'
+adders.style.display = 'flex';
+
+const workTimeView = document.getElementById('workTime');
+const addWork = document.getElementById('addWorkTime');
+const subtractWork = document.getElementById('subtractWork');
+
+const breakTimeView = document.getElementById('breakTime');
+const addBreak = document.getElementById('addBreak');
+const subtractBreak = document.getElementById('subtractBreak');
+
 const ticker = document.getElementById('ticker');
 const playToggleBtn = document.getElementById('pause');
-playToggleBtn.addEventListener('click', () => paused = !paused)
+
+
 
 const state = {
   workTime: 30,
@@ -14,10 +29,26 @@ const state = {
 };
 
 let { workTime, breakTime, time, paused, working } = state;
+const updateTimes = () => {
+  breakTimeView.innerHTML = breakTime;
+  workTimeView.innerHTML = workTime;
+}
+const inc = time => {
+  time === 'workTime' ? workTime++ : breakTime++;
+  updateTimes();
+}
+const dec = time => {
+  time === 'workTime' ? workTime-- : breakTime--;
+  updateTimes();
+}
+
+playToggleBtn.addEventListener('click', () => paused = !paused);
+addWork.addEventListener('click', () => inc("workTime"));
+subtractWork.addEventListener('click', () => dec("workTime"));
+addBreak.addEventListener('click', () => inc("breakTime"));
+subtractBreak.addEventListener('click', () => dec("breakTime"));
 
 const status = document.getElementById('status');
-
-
 
 const refresh = () => {
   status.innerHTML = working ? "Do work things" : "Take it Easy!!!"
@@ -37,8 +68,11 @@ const refresh = () => {
 
     time++;
     ticker.innerHTML = time;
+    document.body.style.background = working ? "skyblue" : "orangered"
+    ticker.style.color = working ? "orangered" : "skyblue";
   }
 }
 
 const tick = () => setInterval(refresh, 1000);
+updateTimes();
 tick()
